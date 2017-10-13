38d14c3b389bccd14f0b72535d7ddd4db681abb3
tanyong <tanyong@huami.com>
2017-10-13 14:21:10 +0800

    use rollup bundler

---
+++ dist/seed.js
@@ -0,0 +1,26 @@
+/*!
+ * Seed.js v1.0.0
+ * (c) 2017-2017 xiaohan2013 
+ * Released under the MIT License.
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.Seed = factory());
+}(this, (function () { 'use strict';
+
+// export default class Seed {
+//     static install: () => void;
+// }
+
+// Seed.install = install;
+
+
+var index = (function () {
+    console.log('the answer is ' + answer);
+});
+
+return index;
+
+})));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { install } from './install'
 
-export default class Seed {
-    static install: () => void;
-}
+// export default class Seed {
+//     static install: () => void;
+// }
 
-Seed.install = install;
+// Seed.install = install;
+
+
+export default () => {
+    console.log(`the answer is ${answer}`);
+}
